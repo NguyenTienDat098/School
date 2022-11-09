77c620adbf068b05ece1558b7d572aebcfd45e7c
NguyenTienDat098 <dat61222@gmail.com>
2022-11-09 21:54:04 +0700

    th5

--- CSharp-th5/bt13.cs
+++ CSharp-th5/bt13.cs
@@ -9,6 +9,12 @@ namespace Program
 {
     class Program
     {
+        int C(int k, int n)
+        {
+            if (k == 0 || k == n) return 1;
+            if (k == 1) return n;
+            return C(k - 1, n - 1) + C(k, n - 1);
+        }
 
         public static void Main()
         {
@@ -20,15 +26,15 @@ namespace Program
             {
                 for (int j = 0; j <= i; j++)
                 {
-                    if (j == 0 || i == 0)
-                    {
-                        result = 1;
-                    }
-                    else
-                    {
-                        result = result * (i - j + 1) / j;
-                    }
-                    Console.Write(result + " ");
+                    // if (j == 0 || i == 0)
+                    // {
+                    //     result = 1;
+                    // }
+                    // else
+                    // {
+                    //     result = result * (i - j + 1) / j;
+                    // }
+                    Console.Write(C(j, i));
                 }
                 Console.WriteLine();
             }
