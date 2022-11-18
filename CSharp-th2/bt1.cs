3331a14b220335579683c6959cd356b7764e45a7
Nguyen Tien Dat <88647219+NguyenTienDat098@users.noreply.github.com>
2022-11-18 14:46:56 +0700

    Update bt1.cs
    https://slidesgo.com/theme/social-issues-thesis-poverty-and-homelessness#search-famous+people&position-1&results-67&rs=search

--- CSharp-th2/bt1.cs
+++ CSharp-th2/bt1.cs
@@ -2,6 +2,7 @@
  Bai tap 1 chuong 2: Tinh chu vi , dien tich cua hinh tron
  Name: Nguyen Tien Dat
  Date: 29/10/2022
+ https://slidesgo.com/theme/social-issues-thesis-poverty-and-homelessness#search-famous+people&position-1&results-67&rs=search
 */
 
 
