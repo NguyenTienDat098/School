9c1d87866755ac54a3bfb3c0260184c26273b093
Nguyen Tien Dat <88647219+NguyenTienDat098@users.noreply.github.com>
2022-12-08 16:16:20 +0700

    Add files via upload

---
+++ bt3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT3
+{
+    class Program
+    {
+        static void NhapSoNguyenDuong(ref int number, string message)
+        {
+            do
+            {
+                Console.Write(message);
+                int.TryParse(Console.ReadLine(), out number);
+
+            } while (number <= 0);
+        }
+
+        static int UCLN(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            return UCLN(b, a % b);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            NhapSoNguyenDuong(ref a, "Nhap vao tu so: ");
+            NhapSoNguyenDuong(ref b, "Nhap vao mau so: ");
+
+            if (UCLN(a, b) == 1)
+            {
+                Console.WriteLine("Phan so toi gian");
+            }else
+            {
+                Console.WriteLine(a / UCLN(a, b) + "/" + b / UCLN(a, b));
+            }
+        }
+    }
+}
